5544cadd655246000c05d6398dbdc3deaf9b2bdc
Bruno Coronado <bcoronado.morales@gmail.com>
2019-09-03 21:49:56 -0600

    Pruebas

--- impresora/views.py
+++ impresora/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def cola_impresion(request):
+    if request.method == 'GET':
+        return Response(status = status.HTTP_200_OK)
+    elif request.method == 'POST':
+        return Response({'received data': request.data.get('imagen').name})
+
